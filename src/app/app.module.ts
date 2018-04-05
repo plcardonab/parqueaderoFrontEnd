5d1ef65e549a9fd97faf463143c704d9b487a7ae
Pedro Luis Cardona Buenaventura - Ceiba Software <pedro.cardona@ceiba.com.co>
2018-04-04 19:07:07 -0500

    más

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from'@angular/forms';
+import{HttpModule} from'@angular/http'
 
 import { AppComponent } from './app.component';
 import { VehiculoComponent } from './vehiculo/vehiculo.component';
@@ -17,7 +18,8 @@ import { IngresoListComponent } from './vehiculo/ingreso-list/ingreso-list.compo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/vehiculo/shared/vehiculo.service.ts
+++ src/app/vehiculo/shared/vehiculo.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
+import {Http,Response,Headers,RequestOptions,RequestMethod} from'@angular/http';
+import {Observable} from 'rxjs/Observable';
+import'rxjs/add/operator/map';
+import'rxjs/add/operator/toPromise';
+
+
 import {VehiculoModel} from './vehiculo.model';
+
 
 @Injectable()
 export class VehiculoService {
 
   vehiculoSeleccionado = new VehiculoModel();
   
-  constructor() { }
+  constructor(private http:Http) { }
+  postIngresar(vehiculo:VehiculoModel){
+    var body = JSON.stringify(vehiculo);
+    var headerOptions = new Headers({'Content-type':'application/json'});
+    var requestOptions = new RequestOptions({method:RequestMethod.Post, headers :headerOptions});
+    return this.http.post('http://localhost:8090/parquear',body,requestOptions).map(x => x.json());
 
+  }
 }
